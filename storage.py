c43e45fc5222d40b128ee7f6c2e5d7c1ad61cebc
raghava0071 <129929248+raghava0071@users.noreply.github.com>
2025-02-26 00:38:05 +0530

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,48 @@
+from google.cloud import datastore, storage
+import time
+
+datastore_client = datastore.Client()
+
+def get_list_of_files(bucket_name):
+    """Lists all the blobs in the bucket."""
+    print("\n")
+    print("get_list_of_files: "+bucket_name)
+
+    blobs = storage_client.list_blobs(bucket_name)
+    print(blobs)
+    files = []
+    for blob in blobs:
+        files.append(blob.name)
+
+    return files
+
+def upload_file(bucket_name, file_name):
+    """Send file to bucket."""
+    print("\n")
+    print("upload_file: "+bucket_name+"/"+file_name)
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    blob.upload_from_filename(file_name)
+
+    return 
+
+def download_file(bucket_name, file_name):
+    """ Retrieve an object from a bucket and saves locally"""  
+    print("\n")
+    print("download_file: "+bucket_name+"/"+file_name)
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(file_name)
+    blob.download_to_filename(file_name)
+    blob.reload()
+    print(f"Blob: {blob.name}")
+    print(f"Bucket: {blob.bucket.name}")
+    print(f"Storage class: {blob.storage_class}")
+    print(f"Size: {blob.size} bytes")
+    print(f"Content-type: {blob.content_type}")
+    print(f"Public URL: {blob.public_url}")
+
+    return
+
