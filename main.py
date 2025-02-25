e1d63613b72b2708d09172ef7605eca9eb6996ab
raghava0071 <129929248+raghava0071@users.noreply.github.com>
2025-02-26 00:25:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, redirect, request, send_file
+
+os.makedirs('files', exist_ok = True)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    index_html="""
+<form method="post" enctype="multipart/form-data" action="/upload" method="post">
+  <div>
+    <label for="file">Choose file to upload</label>
+    <input type="file" id="file" name="form_file" accept="image/jpeg"/>
+  </div>
+  <div>
+    <button>Submit</button>
+  </div>
+</form>"""    
+
+    for file in list_files():
+        index_html += "<li><a href=\"/files/" + file + "\">" + file + "</a></li>"
+
+    return index_html
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    file = request.files['form_file']  # item name must match name in HTML form
+    file.save(os.path.join("./files", file.filename))
+
+    return redirect("/")
+
+@app.route('/files')
+def list_files():
+    files = os.listdir("./files")
+    jpegs = []
+    for file in files:
+        if file.lower().endswith(".jpeg") or file.lower().endswith(".jpg"):
+            jpegs.append(file)
+    
+    return jpegs
+
+@app.route('/files/<filename>')
+def get_file(filename):
+  return send_file('./files/'+filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
