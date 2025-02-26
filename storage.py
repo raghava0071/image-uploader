72cbfc03efbcd9209e65dc94ad1a91df228d867c
raghava0071 <129929248+raghava0071@users.noreply.github.com>
2025-02-26 12:08:57 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -67,9 +67,9 @@ PROMPT2 = "title for the image keep in 3-4 words"
 def image_desc_json(bucket_name, image_path):
     file = genai.upload_file(image_path, mime_type="image/jpeg")
     response1 = model.generate_content([file, "\n\n", PROMPT1])
-    description = response1['choices'][0]['text'].strip() 
+    description = response1
     response2 = model.generate_content([file, "\n\n", PROMPT2])
-    title = response2['choices'][0]['text'].strip()
+    title = response2
     image_data = {
         "title": title,
         "description": description
